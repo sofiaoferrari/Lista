41f38bf07cf602355637ea13518a00ec93708918
sofiaoferrari <soferrari@fi.uba.ar>
2020-11-11 18:13:38 -0300

    Update 11/11

--- lista.c
+++ lista.c
@@ -39,6 +39,37 @@ void lista_destruir(lista_t* lista) {
         free(lista);
     }
 }
+/*
+ * Funcion que recorre la lista 
+*/
+nodo_t* recorrer_lista(lista_t* lista, size_t posicion) {
+    if (lista->cantidad == 1) return lista->nodo_inicio;
+    nodo_t* nodo_siguiente = lista->nodo_inicio->siguiente;
+    for (int i = 0; i < posicion; i++) {
+        nodo_siguiente = nodo_siguiente->siguiente;
+    }
+    return nodo_siguiente;
+}
+
+int lista_insertar_en_posicion(lista_t* lista, void* elemento, size_t posicion) {
+    if (!lista) return ERROR;
+    if (posicion >= lista->cantidad) {
+        int agregar = lista_insertar(lista, elemento);
+        return agregar;
+    }
+    nodo_t* nodo_aux = calloc(1, sizeof(nodo_t));
+    if (!nodo_aux) {
+        free(nodo_aux);
+        return ERROR;
+    }
+    nodo_aux->elemento = elemento;
+    lista->cantidad ++;
+    nodo_t* nodo_siguiente = recorrer_lista(lista, posicion);
+    nodo_aux->siguiente = nodo_siguiente;
+    nodo_t* nodo_anterior = recorrer_lista(lista, posicion-1);
+    nodo_anterior->siguiente = nodo_aux;
+    return EXITO;
+}
 
 int lista_insertar(lista_t* lista, void* elemento) {
     size_t final = lista->cantidad;
--- pruebas.c
+++ pruebas.c
@@ -12,6 +12,20 @@ void probar_destruir_lista(lista_t* lista) {
     pa2m_afirmar(destruccion,"Puedo destruir la lista");
 }
 
+void pruebo_agregar_elemento_en_posicion_inexistente(lista_t* lista, size_t posicion, char elemento) {
+    int agregar = lista_insertar_en_posicion(lista, &elemento, posicion);
+    if (agregar == 0) agregar = EXITO;
+    else agregar = ERROR;
+    pa2m_afirmar(agregar,"Puedo insertar un elemento a la lista aunque la posicion no exista");
+}
+
+void pruebo_agregar_elemento_en_posicion(lista_t* lista, size_t posicion, char elemento) {
+    int agregar = lista_insertar_en_posicion(lista, &elemento, posicion);
+    if (agregar == 0) agregar = EXITO;
+    else agregar = ERROR;
+    pa2m_afirmar(agregar,"Puedo insertar un elemento a la lista en una posicion existente");
+}
+
 void pruebo_agregar_otro_elemento(lista_t* lista, char elemento) {
     int agregar = lista_insertar(lista, &elemento);
     if (agregar == 0) agregar = EXITO;
@@ -36,12 +50,17 @@ lista_t* probar_creacion() {
 
 int main(){
     pa2m_nuevo_grupo("PRUEBAS DE LISTA");
-    lista_t* lista_nueva = probar_creacion();
-    if(!lista_nueva) return ERROR;
-    pruebo_agregar_elemento(lista_nueva, 'a');
-    pruebo_agregar_otro_elemento(lista_nueva, 'b');
+    lista_t* lista_de_prueba = probar_creacion();
+    if(!lista_de_prueba) return ERROR;
+    pruebo_agregar_elemento(lista_de_prueba, 'a');
+    pruebo_agregar_otro_elemento(lista_de_prueba, 'b');
+    pruebo_agregar_otro_elemento(lista_de_prueba, 'f');
+    pruebo_agregar_elemento_en_posicion(lista_de_prueba,1, 'c');
+    pruebo_agregar_elemento_en_posicion(lista_de_prueba,2, 'd');
+    pruebo_agregar_elemento_en_posicion_inexistente(lista_de_prueba,5, 'd');
+    printf("\n Cant d elem: %ld", lista_de_prueba->cantidad);
 
-    probar_destruir_lista(lista_nueva);
+    probar_destruir_lista(lista_de_prueba);
 
     pa2m_nuevo_grupo("PRUEBAS DE PILA");
 
